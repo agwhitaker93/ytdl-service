b34a5bc6f2c9cbdb7454a19f15a84c9fa657e67f
Andrew Whitaker <agwhitaker-dev@protonmail.com>
2025-02-27 00:20:45 +0000

    eslint and prettier should just work based on gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
 import eslintPluginSvelte from "eslint-plugin-svelte";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -9,7 +16,5 @@ export default [
   ...eslintPluginSvelte.configs.recommended,
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  {
-    ignores: [".astro"],
-  },
+  includeIgnoreFile(gitignorePath),
 ];
