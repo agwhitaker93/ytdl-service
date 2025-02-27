cb8603143724609f38c0e2e351e4274f73ae0ee3
Andrew Whitaker <agwhitaker-dev@protonmail.com>
2025-02-27 00:22:38 +0000

    add react and tailwind too for the hell of it

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,18 @@ import node from "@astrojs/node";
 
 import db from "@astrojs/db";
 
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), db()],
+  integrations: [svelte(), db(), react()],
 
   adapter: node({
     mode: "standalone",
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
